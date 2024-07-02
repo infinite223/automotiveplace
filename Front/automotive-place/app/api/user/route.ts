699d6e3aedde5b4a872b2fac691f5f56e2b0f501
Dawid Szmigiel <Dawidszmigiel9@gmail.com>
2024-07-02 21:44:59 +0200

    add user validation in api, create new model, update models and more

--- Front/automotive-place/app/api/user/route.ts
+++ Front/automotive-place/app/api/user/route.ts
@@ -1,5 +1,9 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import { TValidResult } from "@/app/utils/types";
+import { ICreateNotification } from "@/app/components/logger/Notification";
+import { log } from "console";
 
 export async function POST(req: Request) {
   try {
@@ -7,27 +11,115 @@ export async function POST(req: Request) {
 
     const { email, name, password } = body;
 
-    const existingUserByEmail = await prisma.user.findUnique({ where: email });
-    if (existingUserByEmail) {
-      return NextResponse.json({
-        user: null,
-        message: "User not exist with this email",
-        status: 409,
-      });
-    }
-
-    const existingUserByUserName = await prisma.user.findUnique({
-      where: name,
+    const { valid, notification } = await validUserIfExistInDatabse({
+      email,
+      name,
     });
-    if (existingUserByUserName) {
-      return NextResponse.json({
-        user: null,
-        message: "User not exist with this name",
-        status: 409,
-      });
+
+    if (valid) {
+      const { notification, user } = await createNewUser(name, email, password);
+
+      return NextResponse.json({ user, notification }, { status: 201 });
     }
 
-    return NextResponse.json(body);
-  } catch (error) {}
-  return NextResponse.json({ success: true });
+    return NextResponse.json({ user: null, notification }, { status: 401 });
+  } catch (error) {
+    log(error);
+
+    const notification: ICreateNotification = {
+      log: {
+        date: new Date(),
+        status: "Information",
+        title: "Nieoczekiwany błąd, spróbuj ponownie później",
+      },
+      timer: 2000,
+    };
+    return NextResponse.json({ user: null, notification }, { status: 400 });
+  }
 }
+
+export const validUserIfExistInDatabse = async ({
+  email,
+  name,
+}: {
+  name: string;
+  email: string;
+}) => {
+  // valid email
+  const existingUserByEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUserByEmail) {
+    return {
+      valid: false,
+      notification: {
+        log: {
+          date: new Date(),
+          status: "Information",
+          message: "Ten email jest już używany w aplikacji",
+        },
+        timer: 3000,
+      },
+    } as TValidResult;
+  }
+
+  // valid userName
+  const existingUserByUserName = await prisma.user.findUnique({
+    where: { name },
+  });
+
+  if (existingUserByUserName) {
+    return {
+      valid: false,
+      notification: {
+        log: {
+          date: new Date(),
+          status: "Information",
+          message: "Nazwa użytkownika jest już zajęta",
+        },
+        timer: 3000,
+      },
+    } as TValidResult;
+  }
+
+  return {
+    valid: true,
+  } as TValidResult;
+};
+
+const createNewUser = async (name: string, email: string, password: string) => {
+  try {
+    const hashPassword = await hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+      },
+    });
+
+    const notification: ICreateNotification = {
+      log: {
+        date: new Date(),
+        status: "Success",
+        title: "Pomyślnie utworzono konto",
+      },
+      timer: 3000,
+    };
+
+    return { user: newUser, notification };
+  } catch (error) {
+    log(error);
+    const notification: ICreateNotification = {
+      log: {
+        date: new Date(),
+        status: "Information",
+        title: "Coś poszło nie tak",
+      },
+      timer: 3000,
+    };
+
+    return { user: null, notification };
+  }
+};
