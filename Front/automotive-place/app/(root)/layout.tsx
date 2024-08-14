e612f98e0203007c507bf8627919a9e367cb6a02
Dawid Szmigiel <Dawidszmigiel9@gmail.com>
2024-08-14 22:52:58 +0200

    add landing page

--- Front/automotive-place/app/(root)/layout.tsx
+++ Front/automotive-place/app/(root)/layout.tsx
@@ -2,17 +2,21 @@ import type { Metadata } from "next";
 import { HomeHeader } from "./HomeHeader";
 import { SideBar } from "../components/shared/Sidebar";
 import { redirect } from "next/navigation";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Główna strona automotiveplace",
 };
 
-export default function Layout({
+export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getLoggedInUser();
+  if (!user) redirect("/preview");
+
   return (
     <div className="flex h-screen flex-col bg-custom-primary text-custom-primary">
       <HomeHeader />
--- Front/automotive-place/app/(root)/page.tsx
+++ Front/automotive-place/app/(root)/page.tsx
@@ -4,9 +4,6 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const user = await getLoggedInUser();
-  if (!user) redirect("/sign-in");
-
   return (
     <div className="flex w-full items-center justify-center">
       <HomeMainContent contentData={contentData} />
