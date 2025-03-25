45dba6784c251e9922c21bc001f3d79dfe5233bf
infinite223 <dawidszmigiel9@gmail.com>
2025-03-25 18:19:49 +0100

    fix build

--- Front/automotive-place/app/api/content/get-seen/route.ts
+++ Front/automotive-place/app/api/content/get-seen/route.ts
@@ -1,40 +1,38 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import redis from "@/lib/redis";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import { NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") return res.status(405).end();
-  const userData = await getLoggedInUser();
-
-  if (!userData) {
-    return NextResponse.json(
-      { message: "YouMustBeLoggedInToUseThisFunctionality" },
-      { status: 404, statusText: "Unauthorized" }
-    );
-  }
-
-  const { userId, seenPostIds } = req.body;
-
-  if (!userId || !Array.isArray(seenPostIds)) {
-    return res.status(400).json({
-      message: "UserId and seenPostIds are required and should be valid",
-    });
-  }
-
+export async function POST(req: NextRequest) {
   try {
+    const userData = await getLoggedInUser();
+    if (!userData) {
+      return NextResponse.json(
+        { message: "You must be logged in to use this functionality" },
+        { status: 401 }
+      );
+    }
+
+    const body = await req.json();
+    const { userId, seenPostIds } = body;
+
+    if (!userId || !Array.isArray(seenPostIds)) {
+      return NextResponse.json(
+        { message: "UserId and seenPostIds are required and should be valid" },
+        { status: 400 }
+      );
+    }
+
     const redisKey = `user:seenContent:${userId}`;
 
     await redis.sAdd(redisKey, seenPostIds);
+    await redis.expire(redisKey, 86400);
 
-    await redis.expire(redisKey, 86400); // TTL 24 godziny
-
-    res.status(200).json({ message: "Seen content updated successfully" });
+    return NextResponse.json({ message: "Seen content updated successfully" });
   } catch (error) {
     console.error("Error updating seen content:", error);
-    res.status(500).json({ message: "Error updating seen content" });
+    return NextResponse.json(
+      { message: "Error updating seen content" },
+      { status: 500 }
+    );
   }
 }
