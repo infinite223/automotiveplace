101d2210ae386f0672cea1775eef2a513f63dfa1
infinite223 <dawidszmigiel9@gmail.com>
2025-12-25 22:38:27 +0100

    test

--- Front/automotive-place/app/components/createPost/Validation.ts
+++
@@ -1,8 +0,0 @@
-import { TValidResult } from "@/app/utils/types";
-import { TPostCreate } from "@/app/utils/types/post";
-
-export const validPost = (newPost: TPostCreate) => {
-  let validResults: TValidResult[] = [];
-  // TODO - update validation
-  return validResults;
-};
