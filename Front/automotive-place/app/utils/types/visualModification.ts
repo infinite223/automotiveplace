27a874b60681949a4ad619c3b0f3c7db6d5443ef
infinite223 <dawidszmigiel9@gmail.com>
2026-02-24 22:11:23 +0100

    fix build

---
+++ Front/automotive-place/app/utils/types/visualModification.ts
@@ -0,0 +1,42 @@
+export enum TVisualModificationType {
+  FRONT_BUMPER = "FRONT_BUMPER",
+  REAR_BUMPER = "REAR_BUMPER",
+  SPOILER = "SPOILER",
+  SIDE_SKIRT = "SIDE_SKIRT",
+  HOOD = "HOOD",
+  ROOF = "ROOF",
+  WRAP = "WRAP",
+  PAINT = "PAINT",
+  WHEELS = "WHEELS",
+  INTERIOR = "INTERIOR",
+  LIGHTING = "LIGHTING",
+  OTHER = "OTHER",
+}
+
+export type TVisualModification = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  description?: string;
+  modificationType: TVisualModificationType;
+  projectId: string;
+  authorId: string;
+  companyId?: string;
+  isVisible: boolean;
+  images: string[];
+};
+
+export type TVisualModificationCreate = Pick<
+  TVisualModification,
+  "name" | "description" | "modificationType" | "isVisible" | "companyId"
+> & {
+  media?: File[] | string[];
+};
+
+export type TBasicVisualModification = Pick<
+  TVisualModification,
+  "id" | "name" | "modificationType" | "isVisible"
+> & {
+  thumbnailUrl?: string;
+};
